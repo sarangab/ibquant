cc4033fe399a8dcd6c893d8dfbc7779709fbd907
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-08-15 22:33:02 -0400

    update cli

--- ibtrader/cli/console.py
+++ ibtrader/cli/console.py
@@ -12,18 +12,68 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import os
+
 import click
+from rich import print as rprint
+from rich.prompt import Confirm, Prompt
+
+IBC_LATEST = "3.14.0"
 
 
 @click.group()
-def ibtrader():
+def main():
     pass
+
+
+# ---------------
+# main commands
+# ---------------
+@main.command("login")
+def login():
+    print()
+    os.environ["USER"] = Prompt.ask("Please enter your [green]username[/green]")
+    os.environ["PASSWORD"] = Prompt.ask("Please enter your [green]password[/green]")
+    rprint(f"You entered: [green]{os.environ['USER']}[/green] and [green]{os.environ['PASSWORD']}[/green]")
+    print()
+
+
+@main.command("get-ibc")
+def get_ibc():
+    valid_os = dict(mac="IBCMacos", windows="IBCWin", linux="IBCLinux")
+    print()
+    _os = Prompt.ask("Please confirm your operating system", choices=["mac", "windows", "linux"])
+    rprint(f"You entered: [green]{_os}[green]")
+    _os = valid_os[_os]
+    url = f"https://github.com/IbcAlpha/IBC/releases/download/{IBC_LATEST}/{_os}-{IBC_LATEST}.zip"
+    rprint(f"This will install: [green]{url}[green]")
+    confirmed = Confirm.ask("Do you wish to continue?")
+    # TODO use wget to fetch download
+    if confirmed:
+        rprint("[green]confirmed[green]")
+    if not confirmed:
+        rprint("[red]exiting[red]")
+    print()
+
+
+# ---------------
+# advisor commands
+# ---------------
+@main.group()
+def advisor():
+    pass
+
+
+@advisor.command("get-group")
+@click.argument("group")
+def get_group(group):
+    rprint(f"You entered group name: [green]{group}[green]")
 
 
 # ---------------
 # account commands
 # ---------------
-@ibtrader.group()
+@main.group()
 def account():
     pass
 
@@ -51,24 +101,24 @@ def daily_pnl():
 # ---------------
 # trade commands
 # ---------------
-@ibtrader.group()
-def trade():
+@main.group()
+def trader():
     pass
 
 
-@trade.command("trade")
-@click.option("strategy")
-@click.option("market")
-@click.option("contracts")
-@click.option("ordertype")
-@click.option("logger")
-def run_trader(strategy, market, contracts, ordertype, logger):
+@trader.command("trade")
+@click.argument("strategy")
+@click.argument("market")
+@click.argument("contracts")
+@click.argument("ordertype")
+@click.argument("logger")
+def trade(strategy, market, contracts, ordertype, logger):
     pass
 
 
-@trade.command("plot")
-@click.option("market")
-@click.option("indicators")
+@trader.command("plot")
+@click.argument("market")
+@click.argument("indicators")
 def plot(market, indicators):
     pass
 
@@ -76,28 +126,28 @@ def plot(market, indicators):
 # ---------------
 # backtest commands
 # ---------------
-@ibtrader.group()
+@main.group()
 def backtest():
     pass
 
 
 @backtest.command("fetch-data")
-@click.option("-contract")
-@click.option("-end-date")
-@click.option("-duration")
-@click.option("-barsize")
-@click.option("-show")
-@click.option("-use-rth")
-@click.option("-dateformat")
-@click.option("-keep-updated")
+@click.argument("contract")
+@click.argument("end-date")
+@click.argument("duration")
+@click.argument("barsize")
+@click.argument("show")
+@click.argument("use-rth")
+@click.argument("dateformat")
+@click.argument("keep-updated")
 def fetch_data():
     pass
 
 
 @backtest.command("run")
-@click.option("max-drawdown")
-@click.option("long-short")
-@click.option("execution-window")
+@click.argument("max-drawdown")
+@click.argument("long-short")
+@click.argument("execution-window")
 def run_backtest(maxdd, long_short, execution_window):
     pass
 
