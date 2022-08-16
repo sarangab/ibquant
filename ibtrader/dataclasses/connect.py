9e8e25ee576a644c8088174f4dfa1b35543aa5d7
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-08-16 10:39:22 -0400

    refactor

--- ibtrader/dataclasses/connect.py
+++ ibtrader/dataclasses/connect.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 @dataclass
-class ConnectKey:
-    socketport = dict(tws=dict(paper=7497, live=7496), gateway=dict(paper=4002, live=4001))
-    localhost = "127.0.0.1"
+class Socket:
+    port = dict(tws=dict(paper=7497, live=7496), gateway=dict(paper=4002, live=4001))
+    host = "127.0.0.1"
     clientID = np.random.randint(0, 100000)
