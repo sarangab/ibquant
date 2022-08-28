c90226ec5eda5a89a7136af141c8f5f277608325
Justin Goheen <26209687+JustinGoheen@users.noreply.github.com>
2022-08-28 16:25:14 -0400

    add OrderMixin

--- ibquant/mixins/__init__.py
+++ ibquant/mixins/__init__.py
@@ -3,3 +3,4 @@ from ibquant.mixins.advisor import AdvisorMixin
 from ibquant.mixins.connect import ConnectionMixin
 from ibquant.mixins.contract import ContractMixin
 from ibquant.mixins.data import DataMixin
+from ibquant.mixins.order import OrderMixin
--- ibquant/mixins/order.py
+++ ibquant/mixins/order.py
@@ -11,3 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
+from abc import ABC
+
+
+class OrderMixin(ABC):
+    def __init__(self) -> None:
+        super().__init__()
